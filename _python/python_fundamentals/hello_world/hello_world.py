b53cf10cca325a7f5ed925b589504ff4376ff0ba
habibGrande <snomo22dy@gmail.com>
2023-11-07 14:23:27 +0200

    adding asg

---
+++ _python/python_fundamentals/hello_world/hello_world.py
@@ -0,0 +1,19 @@
+print ("hello world")
+
+name ="habib"
+print ("hello" , name)
+print("hello" + name)
+
+favNum = 31
+print ("hello", favNum )
+# print ("hello" + favNum )
+
+favFood1 = "sushi" 
+favFood2 ="shrimp"
+print ("I love to eat {} and {}".format(favFood1,favFood2))
+#2
+print(f"I love to eat {favFood1} and {favFood2}")
+
+favoritNumber = "my favorit number is %d" % 31
+massege = "what about you? "
+print(favoritNumber,massege)
