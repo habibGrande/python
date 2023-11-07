87913e8dac6db1b94583e3fb79345b3be8fa8eb3
habibGrande <snomo22dy@gmail.com>
2023-11-07 13:56:58 +0200

    adding asg

--- _python/python_fundamentals/for_loop_basic1/Looping.py
+++ _python/python_fundamentals/for_loop_basic1/Looping.py
@@ -1,2 +1,27 @@
-for x in range(0,151):
-    print (x)
+for i in range(151):
+    print(i)
+
+for b in range(5,1001,5):
+    print(b)
+ 
+for a in range(1,101):
+    if a%10==0:
+        print("coding")
+    elif a%5==0:
+        print("Coding Dojo")
+
+    else   :
+        print(a)
+valus=0
+for x in range(0,500001,2):
+    valus=valus+1
+    print(x)
+
+for m in range(2018,0,-4):
+    print(m)
+lownum=2
+highnum=9
+mult=3
+for k in range(lownum,highnum+1):
+    if k%mult==0:
+        print(k)
