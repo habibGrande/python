0509705bd41309c795ddf123b1131ae99bdf30a8
habibGrande <snomo22dy@gmail.com>
2023-11-12 14:31:52 +0200

    flask first assignment

--- _python/OOP/UsersWithBankAccounts/users_bank_accounts.py
+++ _python/OOP/UsersWithBankAccounts/users_bank_accounts.py
@@ -1,23 +1,24 @@
 class BankAccount:
-           
+        #    constracters
     def __init__(self,int_rate,balance):
          self.int_rate = int_rate
          self.balance = balance
-         
+        #  methods
     def withdraw(self, amount):
         self.balance -= amount
-        return self
+        
 
     def deposit(self,amount):
         self.balance += amount
-        return self
+        
 
     def yield_interest(self): 
         self.balance += self.balance * self.int_rate
-        return self
+        
         
     def display_account_info(self):
         print( 'Balance: ' , self.balance)
+        return self.balance
 
 
 class User:
@@ -40,12 +41,12 @@ class User:
     def disply_user_balance(self):
         print(f"User: {self.user_name}, Balance: {self.account.display_account_info()}")     
     
-
+newUser = User('ahmad','ahmad@gmail.com')
     
        
 
 habibUser = User('habib','ahmad@axsos.com')
 habibUser.Deposit(56000)
+habibUser.make_withdrawal(2050)
 habibUser.disply_user_balance()
-
 
