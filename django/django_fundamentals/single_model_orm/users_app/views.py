831b25ea06cb16305110876a69f231a8f7f76613
habibGrande <snomo22dy@gmail.com>
2023-11-22 10:46:50 +0200

    users with templates asg

--- django/django_fundamentals/single_model_orm/users_app/views.py
+++ django/django_fundamentals/single_model_orm/users_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , HttpResponse ,redirect
+from .models import User
 # Create your views here.
+
+def root(request):
+    users = User.objects.all()
+
+    context ={
+        'users' : users
+    }
+    return render (request,"index.html", context)
+
+def process(request):
+    firstName = request.POST['firstName']
+    lastName = request.POST ['LastName']
+    email = request.POST['email']
+    age = int(request.POST['age'])
+    User.objects.create(first_name = firstName,last_name =lastName,email_address = email,age = age)
+    return redirect ('/')
