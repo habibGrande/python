7f6a75e7c97cff9a0c80c081a8dd0f3e7efb7439
habibGrande <snomo22dy@gmail.com>
2023-12-01 15:13:22 +0200

    login form

--- django/django_fullstack/login_project/login_app/urls.py
+++ django/django_fullstack/login_project/login_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.root)
+    path('',views.root),
+    path('register',views.create_user),
+    path('login',views.login)
 ]
--- django/django_fullstack/login_project/login_app/views.py
+++ django/django_fullstack/login_project/login_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
+import bcrypt
+from django.shortcuts import render, redirect, HttpResponse
 from .models import *
+from django.contrib import messages
 # Create your views here.
 def root(request):
     users = Users.objects.all()
@@ -9,10 +11,31 @@ def root(request):
     return render(request,'index.html', context)
 
 def create_user(request):
-    first_name = request.POST['first_name']
-    last_name = request.POST['last_name']
-    email = request.POST['email']
-    password = request.POST['password']
-    confirm_pass = request.POST['conf-pass']
-    users = users.objects.create(first_name = first_name, last_name =last_name, email   = email, password = password, confirm_passowrd = confirm_pass)
-    return redirect('/')
+    errors = Users.objects.basic_validator(request.POST)
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request,value,extra_tags=key) 
+        return redirect('/')
+    else:
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_pass = request.POST['conf-pass']
+        pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        users = Users.objects.create(first_name = first_name, last_name =last_name, email   = email, passowrd = pw_hash , confirm_passowrd = pw_hash)
+        print(pw_hash)
+        return redirect('/')
+
+def login(request):
+    user = Users.objects.get(email=request.POST['email-login'])
+    
+    if bcrypt.checkpw(request.POST['password-login'].encode(), user.passowrd.encode()):
+        users_name = Users.objects.all()
+        return render(request,'success.html',users_name)
+    else:
+        return redirect('/')
+
+
+
